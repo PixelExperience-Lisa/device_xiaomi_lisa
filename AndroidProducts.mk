7cfca27af7439ad945e1e80ec60a82efae4ee66a
saikiran2001 <bjsaikiran@gmail.com>
2022-10-27 00:58:09 +0000

    lisa: Pixel Experience bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/awaken_lisa.mk
+    $(LOCAL_DIR)/aosp_lisa.mk
 
 COMMON_LUNCH_CHOICES := \
-    awaken_lisa-user \
-    awaken_lisa-userdebug \
-    awaken_lisa-eng
+    aosp_lisa-user \
+    aosp_lisa-userdebug \
+    aosp_lisa-eng
--- aosp_lisa.mk
+++ aosp_lisa.mk
@@ -12,12 +12,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/lisa/device.mk)
 
 # Inherit some common awaken stuff.
-$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # GApps
 TARGET_GAPPS_ARCH := arm64
-USE_GAPPS := true
 
 # Google Recorder
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
@@ -25,11 +24,13 @@ TARGET_SUPPORTS_GOOGLE_RECORDER := true
 # Google Assistant
 TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
 
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lisa
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := 2109119DG
-PRODUCT_NAME := awaken_lisa
+PRODUCT_NAME := aosp_lisa
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
